b7dce7f79330737d142efeede6ba49f907f0bc65
TasminaAkterMonika <tasminaaktermonika@gmail.com>
2022-02-22 17:24:19 +0600

    first commit

---
+++ resources/views/contactus/form.blade.php
@@ -0,0 +1,119 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="page-header"><h2> {{ $pageTitle }} <small> {{ $pageNote }} </small> </h2></div>
+
+	{!! Form::open(array('url'=>'contactus?return='.$return, 'class'=>'form-horizontal validated sximo-form','files' => true ,'id'=> 'FormTable' )) !!}
+	<div class="toolbar-nav">
+		<div class="row">
+			
+			<div class="col-md-6 " >
+				<div class="submitted-button">
+					<button name="apply" class="tips btn btn-sm btn-default  "  title="{{ __('core.btn_back') }}" ><i class="fa  fa-check"></i> {{ __('core.sb_apply') }} </button>
+					<button name="save" class="tips btn btn-sm btn-default"  id="saved-button" title="{{ __('core.btn_back') }}" ><i class="fa  fa-paste"></i> {{ __('core.sb_save') }} </button> 
+				</div>	
+			</div>
+			<div class="col-md-6 text-right " >
+				<a href="{{ url($pageModule.'?return='.$return) }}" class="tips btn btn-default  btn-sm "  title="{{ __('core.btn_back') }}" ><i class="fa  fa-times"></i></a> 
+			</div>
+		</div>
+	</div>	
+
+
+	<div class="p-5">
+		<ul class="parsley-error-list">
+			@foreach($errors->all() as $error)
+				<li>{{ $error }}</li>
+			@endforeach
+		</ul>		
+		<div class="row">
+	<div class="col-md-12">
+						<fieldset><legend> Contact Us</legend>
+									
+									  <div class="form-group row  " >
+										<label for="Id" class=" control-label col-md-4 text-left"> Id </label>
+										<div class="col-md-6">
+										  <input  type='text' name='id' id='id' value='{{ $row['id'] }}' 
+						     class='form-control form-control-sm ' /> 
+										 </div> 
+										 <div class="col-md-2">
+										 	
+										 </div>
+									  </div> 					
+									  <div class="form-group row  " >
+										<label for="City" class=" control-label col-md-4 text-left"> City </label>
+										<div class="col-md-6">
+										  <input  type='text' name='city' id='city' value='{{ $row['city'] }}' 
+						     class='form-control form-control-sm ' /> 
+										 </div> 
+										 <div class="col-md-2">
+										 	
+										 </div>
+									  </div> 					
+									  <div class="form-group row  " >
+										<label for="Office Description" class=" control-label col-md-4 text-left"> Office Description </label>
+										<div class="col-md-6">
+										  <input  type='text' name='office_description' id='office_description' value='{{ $row['office_description'] }}' 
+						     class='form-control form-control-sm ' /> 
+										 </div> 
+										 <div class="col-md-2">
+										 	
+										 </div>
+									  </div> 					
+									  <div class="form-group row  " >
+										<label for="Contact Description" class=" control-label col-md-4 text-left"> Contact Description </label>
+										<div class="col-md-6">
+										  <input  type='text' name='contact_description' id='contact_description' value='{{ $row['contact_description'] }}' 
+						     class='form-control form-control-sm ' /> 
+										 </div> 
+										 <div class="col-md-2">
+										 	
+										 </div>
+									  </div> 					
+									  <div class="form-group row  " >
+										<label for="Working  Description" class=" control-label col-md-4 text-left"> Working  Description </label>
+										<div class="col-md-6">
+										  <input  type='text' name='working _description' id='working _description' value='{{ $row['working _description'] }}' 
+						     class='form-control form-control-sm ' /> 
+										 </div> 
+										 <div class="col-md-2">
+										 	
+										 </div>
+									  </div> 					
+									  <div class="form-group row  " >
+										<label for="Status" class=" control-label col-md-4 text-left"> Status </label>
+										<div class="col-md-6">
+										  <input  type='text' name='status' id='status' value='{{ $row['status'] }}' 
+						     class='form-control form-control-sm ' /> 
+										 </div> 
+										 <div class="col-md-2">
+										 	
+										 </div>
+									  </div> </fieldset></div>
+	
+		</div>
+
+		<input type="hidden" name="action_task" value="save" />
+		
+		</div>
+	</div>		
+	{!! Form::close() !!}
+		 
+   <script type="text/javascript">
+	$(document).ready(function() { 
+		
+		
+		 	
+		 	 
+
+		$('.removeMultiFiles').on('click',function(){
+			var removeUrl = '{{ url("contactus/removefiles?file=")}}'+$(this).attr('url');
+			$(this).parent().remove();
+			$.get(removeUrl,function(response){});
+			$(this).parent('div').empty();	
+			return false;
+		});		
+		
+	});
+	</script>		 
+@stop
---
+++ resources/views/contactus/public/form.blade.php
@@ -0,0 +1,108 @@
+
+
+		 {!! Form::open(array('url'=>'contactus', 'class'=>'form-horizontal','files' => true , 'parsley-validate'=>'','novalidate'=>' ')) !!}
+
+	@if(Session::has('messagetext'))
+	  
+		   {!! Session::get('messagetext') !!}
+	   
+	@endif
+	<ul class="parsley-error-list">
+		@foreach($errors->all() as $error)
+			<li>{{ $error }}</li>
+		@endforeach
+	</ul>		
+
+
+<div class="col-md-12">
+						<fieldset><legend> Contact Us</legend>
+									
+									  <div class="form-group row  " >
+										<label for="Id" class=" control-label col-md-4 text-left"> Id </label>
+										<div class="col-md-6">
+										  <input  type='text' name='id' id='id' value='{{ $row['id'] }}' 
+						     class='form-control form-control-sm ' /> 
+										 </div> 
+										 <div class="col-md-2">
+										 	
+										 </div>
+									  </div> 					
+									  <div class="form-group row  " >
+										<label for="City" class=" control-label col-md-4 text-left"> City </label>
+										<div class="col-md-6">
+										  <input  type='text' name='city' id='city' value='{{ $row['city'] }}' 
+						     class='form-control form-control-sm ' /> 
+										 </div> 
+										 <div class="col-md-2">
+										 	
+										 </div>
+									  </div> 					
+									  <div class="form-group row  " >
+										<label for="Office Description" class=" control-label col-md-4 text-left"> Office Description </label>
+										<div class="col-md-6">
+										  <input  type='text' name='office_description' id='office_description' value='{{ $row['office_description'] }}' 
+						     class='form-control form-control-sm ' /> 
+										 </div> 
+										 <div class="col-md-2">
+										 	
+										 </div>
+									  </div> 					
+									  <div class="form-group row  " >
+										<label for="Contact Description" class=" control-label col-md-4 text-left"> Contact Description </label>
+										<div class="col-md-6">
+										  <input  type='text' name='contact_description' id='contact_description' value='{{ $row['contact_description'] }}' 
+						     class='form-control form-control-sm ' /> 
+										 </div> 
+										 <div class="col-md-2">
+										 	
+										 </div>
+									  </div> 					
+									  <div class="form-group row  " >
+										<label for="Working  Description" class=" control-label col-md-4 text-left"> Working  Description </label>
+										<div class="col-md-6">
+										  <input  type='text' name='working _description' id='working _description' value='{{ $row['working _description'] }}' 
+						     class='form-control form-control-sm ' /> 
+										 </div> 
+										 <div class="col-md-2">
+										 	
+										 </div>
+									  </div> 					
+									  <div class="form-group row  " >
+										<label for="Status" class=" control-label col-md-4 text-left"> Status </label>
+										<div class="col-md-6">
+										  <input  type='text' name='status' id='status' value='{{ $row['status'] }}' 
+						     class='form-control form-control-sm ' /> 
+										 </div> 
+										 <div class="col-md-2">
+										 	
+										 </div>
+									  </div> </fieldset></div>
+
+			<div style="clear:both"></div>	
+				
+					
+				  <div class="form-group">
+					<label class="col-sm-4 text-right">&nbsp;</label>
+					<div class="col-sm-8">	
+					<button type="submit" name="apply" class="btn btn-default btn-sm" ><i class="fa  fa-check-circle"></i> {{ Lang::get('core.sb_apply') }}</button>
+					<button type="submit" name="submit" class="btn btn-default btn-sm" ><i class="fa  fa-save "></i> {{ Lang::get('core.sb_save') }}</button>
+				  </div>	  
+			
+		</div> 
+		 <input type="hidden" name="action_task" value="public" />
+		 {!! Form::close() !!}
+		 
+   <script type="text/javascript">
+	$(document).ready(function() { 
+		
+		 
+
+		$('.removeCurrentFiles').on('click',function(){
+			var removeUrl = $(this).attr('href');
+			$.get(removeUrl,function(response){});
+			$(this).parent('div').empty();	
+			return false;
+		});		
+		
+	});
+	</script>		 
